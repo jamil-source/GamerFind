b124be1749b42e508e27e520008d889915b38818
jamil-source <jamilbariche9@gmail.com>
2022-02-01 21:11:04 +0100

    GAM-6 create a DataContext class for mapping over entities to database connection Logbook: 2022-02-01

---
+++ Backend/Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet <User> Users { get; set; }
+    }
+}
--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -2,17 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace API
+namespace Backend
 {
     public class Startup
     {
@@ -26,7 +28,6 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
