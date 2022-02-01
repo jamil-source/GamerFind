19ceae9e68b6da7e08451289d97a0fea717e2e2a
jamil-source <jamilbariche9@gmail.com>
2022-02-01 22:20:58 +0100

    GAM-8 create an API controller for Users and a metod that gets all users. Logbook: 2022-02-01

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Data;
+using Backend.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // Get all users
+        // api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+        
+    }
+}
