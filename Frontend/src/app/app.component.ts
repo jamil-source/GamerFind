744a7e1f5a40088cd4c296658bb9b17f940e77bf
jamil-source <jamilbariche9@gmail.com>
2022-02-02 19:12:13 +0100

    GAM-12 import HttpClient that is going to be used for http requests to backend via API (only added on the app.component for now) Logbook: 2022-02-02

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Frontend';
+export class AppComponent implements OnInit{
+  title = 'GamerFind';
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+     
+  }
 }
