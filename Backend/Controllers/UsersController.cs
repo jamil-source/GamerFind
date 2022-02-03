13a57d8d0fd988d2771db2239921d832aa9dbd6a
jamil-source <jamilbariche9@gmail.com>
2022-02-03 20:06:27 +0100

    GAM-16 delete RegisterController no need when we have UsersController. Logbook: 2022-02-03

--- Backend/Controllers/UsersController.cs
+++ Backend/Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Backend.Data;
 using Backend.Entities;
@@ -32,6 +34,26 @@ namespace Backend.Controllers
         public async Task<ActionResult<User>> GetUser(int id)
         {
             var user = await _context.Users.FindAsync(id);
+            return user;
+        }
+
+        // Register User
+        // api/users/register
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> RegisterUser(string username, string email, string password)
+        {
+            using var hmac = new HMACSHA512(); // Hashing 
+            User user = new User
+            {
+                UserName = username,
+                Email = email,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key // HMACSHA512 generates a key and that key will be used as salt for the PW
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            
             return user;
         }
     }
