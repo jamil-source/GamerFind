8e064976ee7a560085cef724964a586fca128941
jamil-source <jamilbariche9@gmail.com>
2022-02-06 21:26:20 +0100

    GM-20 GM-21 create account service with login method. Logbook: 2022-02-06.

---
+++ Frontend/src/app/shared/services/account.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = "http://localhost:5001/api/"
+  constructor(private http: HttpClient) { }
+
+  login(loginObj: any){
+    return this.http.post(`${this.baseUrl}account/login`, loginObj);
+  }
+}
