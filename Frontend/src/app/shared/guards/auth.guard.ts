e87cc60aea8858e201baaaec53ddf7d4d7c0358d
jamil-source <jamilbariche9@gmail.com>
2022-02-08 20:27:46 +0100

    GAM-26 create logic for guards that it observe our loggedInUser observable and checks if user is logged in or not. Logbook: 2022-02-08

--- Frontend/src/app/shared/guards/auth.guard.ts
+++ Frontend/src/app/shared/guards/auth.guard.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AccountService } from '../services/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+  canActivate(): Observable<boolean> {
+    return this.accountService.loggedInUser$.pipe(
+      map(user =>{
+        if(user){
+          return true;
+        }
+        this.toastr.error("You are not authenticated")
+      })
+    )
   }
-  
+
 }
