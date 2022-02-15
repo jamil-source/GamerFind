5a238fb8b09ae7a6d96adee9dfea0f23ac12e44e
jamil-source <jamilbariche9@gmail.com>
2022-02-15 17:56:46 +0100

    GAM-89 add more props for user entity. Logbook: 2022-02-15

--- Backend/Entities/User.cs
+++ Backend/Entities/User.cs
@@ -12,6 +12,15 @@ namespace Backend.Entities
         public string Email { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-
+        public DateTime DateOfBirth { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string GameType { get; set; }
+        public string Description { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
