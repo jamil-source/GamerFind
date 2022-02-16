85f32bac55e890e295f8d7ce9b755f3ff4250f94
jamil-source <jamilbariche9@gmail.com>
2022-02-16 17:23:15 +0100

    GAM-37 create requests methods to retrieve users from server. Create option object to send token with headers. Logbook: 2022-02-16

--- Frontend/src/app/shared/services/members.service.ts
+++ Frontend/src/app/shared/services/members.service.ts
@@ -1,6 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Member } from 'src/app/models/Member';
 import { environment } from 'src/environments/environment';
+
+// We need to send token with the requests
+const httpOpt = {
+  headers: new HttpHeaders({
+    Authorization: `Bearer ${JSON.parse(localStorage.getItem('user')).token}`
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +20,11 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get(`${this.baseUrl}users`)
+    return this.http.get<Member[]>(`${this.baseUrl}users`, httpOpt)
+  }
+
+  getMember(userName: string) {
+    return this.http.get<Member[]>(`${this.baseUrl}users/${userName}`, httpOpt)
+
   }
 }
