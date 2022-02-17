a5f1a67549495779f00e4cf112ba3313c29ce017
jamil-source <jamilbariche9@gmail.com>
2022-02-17 20:00:52 +0100

    GAM-94 GAM-95 add functionality so that submit button works and is disabled when form is dirty. Logbook: 2022-02-17

--- Frontend/src/app/members/member-edit/member-edit.component.ts
+++ Frontend/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/models/Member';
 import { User } from 'src/app/models/User';
@@ -11,10 +13,11 @@ import { MembersService } from 'src/app/shared/services/members.service';
   styleUrls: ['./member-edit.component.scss']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild("editForm") editForm: NgForm;
   member: Member;
   user: User;
 
-  constructor(private accountService: AccountService, private memberService: MembersService) { 
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
     this.accountService.loggedInUser$.pipe(take(1)).subscribe(user => this.user = user);
   }
 
@@ -22,10 +25,17 @@ export class MemberEditComponent implements OnInit {
     this.getLoggedInMember();
   }
 
-  getLoggedInMember(){
+  getLoggedInMember() {
     this.memberService.getMember(this.user.userName).subscribe(member => {
       this.member = member;
     })
+  }
+
+  updateMemberInfo() {
+    console.log(this.member);
+    this.toastr.success("Update")
+    // reset form state, resets save button to disable
+    this.editForm.reset(this.member)
   }
 
 }
