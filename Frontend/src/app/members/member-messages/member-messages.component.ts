959e5792344a843f78055e69402104edcd61c378
jamil-source <jamilbariche9@gmail.com>
2022-03-04 17:17:37 +0100

    GAM-61 add and style members-message template. Logbook: 2022-03-04

--- Frontend/src/app/members/member-messages/member-messages.component.ts
+++ Frontend/src/app/members/member-messages/member-messages.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Message } from 'src/app/models/Message';
 import { MessageService } from 'src/app/shared/services/message.service';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-member-messages',
@@ -10,15 +11,18 @@ import { MessageService } from 'src/app/shared/services/message.service';
 export class MemberMessagesComponent implements OnInit {
   @Input() userName: string
   messages: Message[];
+  faClock = faClock
 
   constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getMessageThread();
   }
 
   getMessageThread(){
     this.messageService.getMessageThread(this.userName).subscribe(res => {
       this.messages = res
+      console.log(this.messages)
     })
   }
 
