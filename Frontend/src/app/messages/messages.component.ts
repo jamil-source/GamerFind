0398e4f37b67e1df555dbb47d4413750a2f34804
jamil-source <jamilbariche9@gmail.com>
2022-03-04 13:19:44 +0100

    GAM-63 create methods in messages component for getting messages. Logbook: 2022-03-04

--- Frontend/src/app/messages/messages.component.ts
+++ Frontend/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../models/Message';
+import { Pagination } from '../models/Pagination';
+import { MessageService } from '../shared/services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.scss']
 })
 export class MessagesComponent implements OnInit {
+  messages: Message[];
+  pagination: Pagination;
+  container: 'Unread'
+  pageNumber = 1;
+  pageSize = 6;
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getMessages()
+  }
+
+  getMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe(res => {
+      this.messages = res.result;
+      this.pagination = res.pagination;
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page
+      this.getMessages();
+    }
   }
 
 }
