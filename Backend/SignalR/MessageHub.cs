dd24df5da0147b1c60ac055a81e517d7c53b1475
jamil-source <jamilbariche9@gmail.com>
2022-03-10 16:33:14 +0100

    GAM-101 GAM-66 create method in MessageHub for sending messages. Logbook: 2022-03-10

--- Backend/SignalR/MessageHub.cs
+++ Backend/SignalR/MessageHub.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Backend.DTO;
+using Backend.Entities;
 using Backend.Extensions;
 using Backend.Interfaces;
 using Microsoft.AspNetCore.SignalR;
@@ -13,9 +15,11 @@ namespace Backend.SignalR
     {
         private readonly IMessageRepository _messageRepository;
         private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
 
-        public MessageHub(IMessageRepository messageRepository, IMapper mapper)
+        public MessageHub(IMessageRepository messageRepository, IMapper mapper, IUserRepository userRepository)
         {
+            _userRepository = userRepository;
             _messageRepository = messageRepository;
             _mapper = mapper;
         }
@@ -28,7 +32,7 @@ namespace Backend.SignalR
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
             var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
-            
+
             await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
         }
 
@@ -37,14 +41,50 @@ namespace Backend.SignalR
             await base.OnDisconnectedAsync(exception);
         }
 
+        public async Task SendMessage(CreateMessageDTO createMessageDTO)
+        {
+            var username = Context.User.GetUsername();
+
+            if (username == createMessageDTO.ReceiverUsername.ToLower())
+            {
+                throw new HubException("Message can not be send to yourself!");
+            }
+
+            var sender = await _userRepository.GetUserByUsernameAsync(username);
+            var receiver = await _userRepository.GetUserByUsernameAsync(createMessageDTO.ReceiverUsername);
+
+            if (receiver == null)
+            {
+                throw new HubException("User not found");
+            }
+
+            var message = new Message
+            {
+                Sender = sender,
+                Receiver = receiver,
+                SenderUsername = sender.UserName,
+                ReceiverUsername = receiver.UserName,
+                Content = createMessageDTO.Content
+            };
+
+            _messageRepository.AddMessage(message);
+
+            if (await _messageRepository.SaveAllAsync())
+            {
+                var group = GetGroupName(sender.UserName, receiver.UserName);
+                await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDTO>(message));
+            }
+
+        }
+
         private string GetGroupName(string caller, string other)
         {
             var stringCompare = string.CompareOrdinal(caller, other) < 0;
-            if(stringCompare)
+            if (stringCompare)
             {
                 return $"{caller}-{other}";
             }
-            else 
+            else
             {
                 return $"{other}-{caller}";
             }
