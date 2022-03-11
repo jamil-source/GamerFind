e804bd8b0d6d755415e2d6183206096718d0ae8a
jamil-source <jamilbariche9@gmail.com>
2022-03-11 11:27:15 +0100

    GAM-65 add new migrations to DB to be able to see who is currently connected to messagehub. Logbook: 2022-03-11

--- Backend/Data/MessageRepository.cs
+++ Backend/Data/MessageRepository.cs
@@ -22,6 +22,11 @@ namespace Backend.Data
             _context = context;
         }
 
+        public void AddGroup(Group group)
+        {
+            _context.Groups.Add(group);
+        }
+
         public void AddMessage(Message message)
         {
             _context.Messages.Add(message);
@@ -32,9 +37,19 @@ namespace Backend.Data
             _context.Messages.Remove(message);
         }
 
+        public async Task<Connection> GetConnection(string connectionId)
+        {
+            return await _context.Connections.FindAsync(connectionId);
+        }
+
         public async Task<Message> GetMessage(int id)
         {
             return await _context.Messages.Include(u => u.Sender).Include(u => u.Receiver).SingleOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<Group> GetMessageGroup(string groupName)
+        {
+            return await _context.Groups.Include(x => x.Connections).FirstOrDefaultAsync(x => x.Name == groupName);
         }
 
         public async Task<IEnumerable<MessageDTO>> GetMessageThread(string currentUsername, string receiverUsername)
@@ -81,6 +96,11 @@ namespace Backend.Data
             var messages = query.ProjectTo<MessageDTO>(_mapper.ConfigurationProvider);
 
             return await PagedList<MessageDTO>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+        }
+
+        public void RemoveConnection(Connection connection)
+        {
+            _context.Connections.Remove(connection);
         }
 
         public async Task<bool> SaveAllAsync()
