f6d4f74cba432836819d78a8799017988a833d58
jamil-source <jamilbariche9@gmail.com>
2022-03-11 12:54:01 +0100

    GAM-68 displat notification on client side if message is received. Logbook: 2022-03-11

--- Frontend/src/app/shared/services/presence.service.ts
+++ Frontend/src/app/shared/services/presence.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { ToastrService } from 'ngx-toastr';
 import { BehaviorSubject } from 'rxjs';
+import { take } from 'rxjs/operators';
 import { User } from 'src/app/models/User';
 import { environment } from 'src/environments/environment';
 
@@ -14,7 +16,7 @@ export class PresenceService {
   private onlineUsersSource = new BehaviorSubject<string[]>([]);
   onlineUsers$ = this.onlineUsersSource.asObservable();
 
-  constructor(private toastr: ToastrService) { }
+  constructor(private toastr: ToastrService, private router: Router) { }
 
   createHubConnection(user: User) {
     this.hubConnection = new HubConnectionBuilder().withUrl(`${this.hubUrl}presence`, {
@@ -32,6 +34,10 @@ export class PresenceService {
 
     this.hubConnection.on("GetOnlineUsers", (userNames: string[]) => {
       this.onlineUsersSource.next(userNames);
+    })
+
+    this.hubConnection.on("NewMessageReceived", ({ userName }) => {
+      this.toastr.info(`${userName} sent you a message!`).onTap.pipe(take(1)).subscribe(() => this.router.navigateByUrl(`/members/${userName}?tab=2`))
     })
   }
 
