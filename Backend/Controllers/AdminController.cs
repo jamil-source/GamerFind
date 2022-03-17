9ce9d37880e19bdda7876e5d503e861a219d89a0
jamil-source <jamilbariche9@gmail.com>
2022-03-17 15:55:18 +0100

    GAM-73 make it possible for admins to get users. Logbook: 2022-03-17

--- Backend/Controllers/AdminController.cs
+++ Backend/Controllers/AdminController.cs
@@ -2,20 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
     public class AdminController : BaseApiController
     {
+        private readonly UserManager<User> _userManager;
+        public AdminController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-
-        public ActionResult GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-            return Ok("Only admins");
+            var users = await _userManager.Users
+                .Include(r => r.UserRoles)
+                .ThenInclude(r => r.Role)
+                .OrderBy(u => u.UserName)
+                .Select(u => new
+                {
+                    u.Id,
+                    Username = u.UserName,
+                    Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
+
+            return Ok(users);
         }
-        
+
     }
+
+
 }
