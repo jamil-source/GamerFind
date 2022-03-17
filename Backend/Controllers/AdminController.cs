c05d73308f7fb94d4c7f28ddc9370e26a0273a27
jamil-source <jamilbariche9@gmail.com>
2022-03-17 16:09:47 +0100

    GAM-75 make it possible for admins to change users role. Logbook: 2022-03-17

--- Backend/Controllers/AdminController.cs
+++ Backend/Controllers/AdminController.cs
@@ -37,6 +37,37 @@ namespace Backend.Controllers
             return Ok(users);
         }
 
+        [HttpPost("edit-roles/{username}")]
+        public async Task<ActionResult> EditRoles(string username, [FromQuery] string roles)
+        {
+            var selectedRoles = roles.Split(",").ToArray();
+
+            var user = await _userManager.FindByNameAsync(username);
+
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Failed to add roles");
+            }
+
+            result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+            if (!result.Succeeded)
+            {
+                return BadRequest("Failed to remove roles");
+            }
+
+            return Ok(await _userManager.GetRolesAsync(user));
+        }
+
     }
 
 
