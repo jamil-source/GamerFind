d73cb1a217f90d4e8e71920eff1beecf97d6e899
jamil-source <jamilbariche9@gmail.com>
2022-03-30 11:10:49 +0200

    GAM-102 fix error page nav problem. Logbook: 2022-03-30

--- Frontend/src/app/error/error.component.ts
+++ Frontend/src/app/error/error.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { faCircleExclamation, faGamepad } from '@fortawesome/free-solid-svg-icons';
 import { SharedService } from '../shared/services/shared.service';
 @Component({
@@ -6,16 +6,20 @@ import { SharedService } from '../shared/services/shared.service';
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss']
 })
-export class ErrorComponent implements OnInit {
-
-  constructor(private shared: SharedService) { }
+export class ErrorComponent implements OnInit, OnDestroy {
   faCircleExclamation = faCircleExclamation
   faGamePad = faGamepad
   showNav: boolean = true;
 
+  constructor(private shared: SharedService) { }
 
   ngOnInit(): void {
     this.showNav = false;
+    this.shared.changeHideNav(this.showNav);
+  }
+
+  ngOnDestroy(): void {
+    this.showNav = true;
     this.shared.changeHideNav(this.showNav);
   }
 
